865e28283a69a495b768dae8e5687cca5a30bd7d
hyunmin200 <gyejeongjin@gmail.com>
2024-08-05 17:51:13 +0900

    클라이언트 컴포넌트 사용

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import classes from "./page.module.css";
-import ImageSlideshow from "@/components/images/image-slidshow";
+import ImageSlideshow from "@/components/images/image-slideshow";
 
 export default function Home() {
   return (
