15b2baa9d914b4aa98ac5a3ede6f9023d6012c69
hyunmin200 <gyejeongjin@gmail.com>
2024-08-06 11:48:51 +0900

    식사 페이지 음식 데이터 불러오기

--- components/meals/meal-item.js
+++ components/meals/meal-item.js
@@ -4,6 +4,8 @@ import Image from "next/image";
 import classes from "./meal-item.module.css";
 
 export default function MealItem({ title, slug, image, summary, creator }) {
+  console.log(title);
+
   return (
     <article className={classes.meal}>
       <header>
--- components/meals/meals-grid.js
+++ components/meals/meals-grid.js
@@ -2,12 +2,15 @@ import MealItem from "./meal-item";
 import classes from "./meals-grid.module.css";
 
 export default function MealsGrid({ meals }) {
+  // meals.map((meal) => console.log(meal));
   return (
     <ul className={classes.meals}>
       {meals.map((meal) => {
-        <li key={meal.id}>
-          <MealItem {...meal} />
-        </li>;
+        return (
+          <li key={meal.id}>
+            <MealItem {...meal} />
+          </li>
+        );
       })}
     </ul>
   );
